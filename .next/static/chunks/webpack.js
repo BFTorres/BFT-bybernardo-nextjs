0339896c8e2189e58fbf3c18bd9b8f76b82c2aa3
BFTorres <bfonseca@gmx.de>
2025-02-18 21:53:43 +0100

    logo size adjustment

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "971519e9afa7bbb1"; }
+/******/ 		__webpack_require__.h = function() { return "5c7a5fde0f1130bb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
